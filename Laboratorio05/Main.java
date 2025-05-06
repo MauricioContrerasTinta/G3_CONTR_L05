4e2de0840cb4e851f2fc339aa48ae3c2f1d9862b
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-05 21:37:19 -0500

    crear instancia del gestor generico

--- Laboratorio05/Main.java
+++ Laboratorio05/Main.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        GestorDeTareas<Tarea> gestor = new GestorDeTareas<>();
+    }
 }
