85a8f21708481ecd993414f0fcb5784173805c61
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-05 19:20:18 -0500

    constructor

--- Laboratorio05/Tarea.java
+++ Laboratorio05/Tarea.java
@@ -4,5 +4,10 @@ public class Tarea {
     private String titulo;
     private int prioridad;
 
+    public Tarea(String titulo, int prioridad){
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+    }
+
     
 }
