2ab74a1f0639370d11f8f8b7320d1fd252cce22d
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-05 21:51:42 -0500

    agregar tareas

--- Laboratorio05/Main.java
+++ Laboratorio05/Main.java
@@ -6,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         GestorDeTareas<Tarea> gestor = new GestorDeTareas<>();
         List<Tarea> tareasCompletadas = new ArrayList<>();
+
+        Tarea t1 = new Tarea("Comer", 2);
+        Tarea t2 = new Tarea("Dormir", 1);
+        Tarea t3 = new Tarea("Estudiar",3 ); 
     }
 }
